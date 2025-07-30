2845a6f530b5fb87781a404d89b47a5cdba38cfe
advay-inabathini <iadvay@gmail.com>
2025-07-31 00:29:28 +0530

    before soft publishng

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://advay-inabathini.github.io",
   integrations: [tailwind()],
 });
