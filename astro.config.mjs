8a93531e2f3ebfa0281d96bd117b2f8b12892dc0
advay-inabathini <iadvay@gmail.com>
2025-07-31 18:30:27 +0530

    Revert astro.config.mjs to local development settings

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,5 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://advay-inabathini.github.io",
   integrations: [tailwind()],
 });
